46d4b0dec3f9259683142e36d5b82fdb778078fa
BearingMe <gomes.bruno201@gmail.com>
2025-07-02 15:19:01 -0300

    fix(backend): cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import users from "./users";
+import subscriptions from "./subscriptions";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 // routers
 app.use("/users", users)
+app.use("/subscriptions", subscriptions)
 
 app.listen(3000, () => console.log("Listening at port 3000"));
