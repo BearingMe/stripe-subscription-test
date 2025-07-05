75446fcb1520190fa645daf9ecfc8ae098a38a8c
BearingMe <gomes.bruno201@gmail.com>
2025-07-05 16:02:44 -0300

    refactor: remove dead code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,13 @@
 import express from "express";
 import users from "./users";
-import subscriptions from "./subscriptions";
 import cors from "cors";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // routers
-app.use("/users", users)
-app.use("/subscriptions", subscriptions)
+app.use("/users", users);
 
 app.listen(3000, () => console.log("Listening at port 3000"));
