2e0794c52bbf6c150ba536df1c751740525fa4b8
BearingMe <gomes.bruno201@gmail.com>
2025-07-05 16:02:52 -0300

    feat: add error handling utilities and VSCode settings

--- server/src/common/utils/errors.ts
+++ server/src/common/utils/errors.ts
@@ -1,5 +1,11 @@
+import { HttpError } from "../exceptions/HttpError";
+
 export function isError(err: unknown): err is Error {
   return err instanceof Error;
+}
+
+export function isHttpError(err: unknown): err is HttpError {
+  return err instanceof HttpError;
 }
 
 export function extractErrorMessage(err: unknown) {
