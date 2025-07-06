cf358e2671940120314531642c3b9fb5d4b22f78
BearingMe <gomes.bruno201@gmail.com>
2025-07-06 18:14:42 -0300

    fix(shared): add endpoints

---
+++ shared/constants/endpoints.ts
@@ -0,0 +1,6 @@
+export const USERS = "/users";
+export const BILLING_CUSTOMERS = "/billing/customers";
+export const BILLING_PAYMENT_METHODS = "/billing/payment-methods";
+export const BILLING_PLANS = "/billing/plans";
+export const BILLING_SUBSCRIPTIONS = "/billing/subscriptions";
+
