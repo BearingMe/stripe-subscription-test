470a1d59b746434826e8e90d3860180b04b76f7e
BearingMe <gomes.bruno201@gmail.com>
2025-07-06 18:14:01 -0300

    feat: create mutations

--- client/src/api/mutations.tsx
+++ client/src/api/mutations.tsx
@@ -1,69 +1,59 @@
+import type { CreateUserDTO } from "../../../shared/schemas/create-user.schema";
+import type { CreateCustomerDTO } from "../../../shared/schemas/create-customer.schema";
+import type { CreateSubscriptionDTO } from "../../../shared/schemas/create-subscripton.schema";
+import type { User } from "../../../shared/entities/user.entity";
+import type { Customer } from "../../../shared/entities/customer.entity";
+import type { Subscription } from "../../../shared/entities/subscription.entity";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import { 
+  USERS, 
+  BILLING_CUSTOMERS, 
+  BILLING_PAYMENT_METHODS, 
+  BILLING_SUBSCRIPTIONS 
+} from "../../../shared/constants/endpoints";
+import http from "@/lib/http";
 
-interface ResponseObject<T> {
-  data: T;
+export function replica() {
+  return useMutation({
+    mutationFn: async (data: unknown) => {
+      const response = await http.options("/", data);
+      return response.data
+    }
+  })
 }
 
 export function useCreateUser() {
-  interface CreateUser {
-    name: string;
-    email: string;
-  }
-
-  interface User {
-    name: string;
-    email: string;
-    uuid: string;
-  }
-
   return useMutation({
-    mutationFn: async (data: CreateUser) => {
-      const url = "http://localhost:3000/users";
-      const response = await axios.post<ResponseObject<User>>(url, data);
-      return response.data;
-    },
-  });
+    mutationFn: async (data: CreateUserDTO) => {
+      const response = await http.post<{ data: User }>(USERS, data);
+      return response.data
+    }
+  })
 }
 
-export function useCreateSubscriptionCustomer() {
-  interface CreateSubscriptionCustomer {
-    name: string;
-    email: string;
-    uuid: string;
-  }
-
-  interface SubscriptionCUstomer {
-    id: string;
-  }
-
+export function useCreateCustomer() {
   return useMutation({
-    mutationFn: async (data: CreateSubscriptionCustomer) => {
-      const url = "http://localhost:3000/subscriptions/customers";
-
-      const response = await axios.post<ResponseObject<SubscriptionCUstomer>>(
-        url,
-        data
-      );
-      return response.data;
-    },
-  });
+    mutationFn: async (data: CreateCustomerDTO) => {
+      const response = await http.post<{ data: Customer }>(BILLING_CUSTOMERS, data);
+      return response.data
+    }
+  })
 }
 
-export function useCheckout() {
-  interface Checkout {
-    stripeCustomerId: string;
-    stripePriceId: string;
-    stripePaymentId: string;
-  }
-
+export function useSetupPaymentMethod() {
   return useMutation({
-    mutationFn: async (data: Checkout) => {
-      const response = await axios.post(
-        "http://localhost:3000/subscriptions/checkout",
-        data
-      );
-      return response.data;
-    },
-  });
+    mutationFn: async (data: unknown) => {
+      const response = await http.post<{ data: string }>(BILLING_PAYMENT_METHODS + "/setup", data);
+      return response.data
+    }
+  })
+}
+
+export function useCreateSubscription() {
+  return useMutation({
+    mutationFn: async (data: CreateSubscriptionDTO) => {
+      const response = await http.post<{ data: Subscription }>(BILLING_SUBSCRIPTIONS, data);
+      return response.data
+    }
+  })
 }
